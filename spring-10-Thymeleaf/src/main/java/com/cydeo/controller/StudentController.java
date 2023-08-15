f8365b0856c66fbf53c73a75fee4f46e9e820a0b
Muhammed <muhammedihsansolak@gmail.com>
2023-08-15 13:14:48 +0300

    Thymeleaf module updated

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student") // localhost:8080/student
@@ -17,5 +18,12 @@ public class StudentController {
         return "student/register";
     }
 
+    @RequestMapping("/welcome") // localhost:8080/student/welcome
+    public String welcome(){
+
+
+
+        return "student/welcome";
+    }
 
 }
