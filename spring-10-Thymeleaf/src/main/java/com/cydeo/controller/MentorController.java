7b5cd1253ae6540f8c4ff202e2f60874b214d02b
Muhammed <muhammedihsansolak@gmail.com>
2023-08-16 10:29:11 +0300

    Thymeleaf module done

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,19 +1,40 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")  // localhost:8080/mentor
 public class MentorController {
 
 
-    @RequestMapping("/register") // localhost:8080/mentor/register
-    public String register(){
+    @GetMapping("/register") // localhost:8080/mentor/register
+    public String register(Model model){
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
 
+        model.addAttribute("mentor",new Mentor());
+        model.addAttribute("batchList",batchList);
 
-        return "student/register";
+        return "mentor/mentor-register";
     }
 
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model){
+
+
+     //   return "mentor/mentor-confirmation";
+        //return "mentor/mentor-register"; //after registration if we want to see the blank page again, we can set the endpoint like this
+
+        return "redirect:/mentor/register";
+    }
 
 }
--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,14 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student") // localhost:8080/student
 public class StudentController {
 
-    @RequestMapping("/register") // localhost:8080/student/register
+   // @RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students" , DataGenerator.createStudent());
@@ -18,7 +18,8 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome") // localhost:8080/student/welcome
+   // @RequestMapping(value = "/welcome", method = RequestMethod.POST) // localhost:8080/student/welcome
+    @PostMapping("/welcome")
     public String welcome(){
 
 
