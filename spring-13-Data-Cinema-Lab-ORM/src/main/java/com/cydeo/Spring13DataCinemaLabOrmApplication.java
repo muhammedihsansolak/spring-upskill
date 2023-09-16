edeb6855cc228fb9fdd77418d83a9e05daf5cd30
Muhammed <muhammedihsansolak@gmail.com>
2023-09-16 10:05:36 +0300

    CinemaLab started

---
+++ spring-13-Data-Cinema-Lab-ORM/src/main/java/com/cydeo/Spring13DataCinemaLabOrmApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13DataCinemaLabOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13DataCinemaLabOrmApplication.class, args);
+    }
+
+}
