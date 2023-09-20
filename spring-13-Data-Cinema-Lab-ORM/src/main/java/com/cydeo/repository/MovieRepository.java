21db64a5ec43186c2eef84f852e4fb4ab06f0af1
Muhammed <muhammedihsansolak@gmail.com>
2023-09-20 12:45:24 +0300

    MovieRepository DONE. derived, native queries and JPQL methods DONE

--- spring-13-Data-Cinema-Lab-ORM/src/main/java/com/cydeo/repository/MovieRepository.java
+++ spring-13-Data-Cinema-Lab-ORM/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Movie;
 
+import com.cydeo.enums.State;
+import com.cydeo.enums.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,39 +21,56 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     //Write a derived query to read a movie with a name
 
+    Optional<Movie> findByName(String name);
 
     //Write a derived query to list all movies between a range of prices
 
+    List<Movie> findByPriceBetween(BigDecimal price1, BigDecimal price2);
 
     //Write a derived query to list all movies where duration exists in the specific list of duration
 
+    List<Movie> getByDurationIn(List<Integer> durations);
 
     //Write a derived query to list all movies with higher than a specific release date
 
+    List<Movie> readAllByReleaseDateAfter(LocalDate date);
 
     //Write a derived query to list all movies with a specific state and type
 
+    List<Movie> readAllByStateAndType(State state, Type type);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movies between a range of prices
 
+    @Query("SELECT m FROM Movie m WHERE m.price between ?1 AND ?2")
+    List<Movie> retrieveAllByPriceBetween(BigDecimal price1, BigDecimal price2);
 
     //Write a JPQL query that returns all movie names
 
+    @Query("SELECT m.name FROM Movie m")
+    List<String> fetchAllNames();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns a movie by name
 
+    @Query(value = "SELECT * FROM movie WHERE name = ?1" ,nativeQuery = true)
+    Optional<Movie> retrieveByName(String name);
 
     //Write a native query that return the list of movies in a specific range of prices
 
+    @Query(value = "SELECT * FROM movie WHERE price BETWEEN ?1 AND ?2" ,nativeQuery = true)
+    List<Movie> fetchAllByPriceBetween(BigDecimal price1, BigDecimal price2);
 
     //Write a native query to return all movies where duration exists in the range of duration
 
+    @Query(value = "SELECT * FROM movie WHERE duration IN ?1" ,nativeQuery = true)
+    List<Movie> fetchAllByDurationBetween(List<Integer> durations);
 
     //Write a native query to list the top 5 most expensive movies
 
+    @Query(value = "SELECT * FROM movie ORDER BY DESC price LIMIT 5" ,nativeQuery = true)
+    List<Movie> fetchTop5ExpensiveMovies();
 
 }
