86f23f7cff490b226ffbc6c86076aedd7f996019
Muhammed <muhammedihsansolak@gmail.com>
2023-11-01 18:18:30 +0300

    OpenAPI3 module created. sample app created

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
@@ -0,0 +1,39 @@
+package com.cydeo;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Spring19RestOpenApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestOpenApi3Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                        .title("Cydeo Application OpenAPI")
+                        .version("v1")
+                        .description("Cydeo API documentation"))
+                .servers(
+                        List.of(new Server()
+                                .url("https://dev.cydeo.com")
+                                .description("Dev Environment")));
+    }
+
+}
