71553f83491d0c344709c303fc6813b5caa26991
Muhammed <muhammedihsansolak@gmail.com>
2023-11-02 09:43:57 +0300

    AccountController and UserController created. Documentation DONE

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
@@ -0,0 +1,38 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.AccountDTO;
+import com.cydeo.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+@Tag(name = "Account", description = "Account CRUD operations")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/list")
+    @Operation(summary = "Read all accounts")
+    public ResponseEntity<List<AccountDTO>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAccounts());
+    }
+
+    @GetMapping
+    @Operation(summary = "Read an account by id")
+    public ResponseEntity<AccountDTO> getById(@RequestParam("id") Long id) throws Exception {
+        return ResponseEntity.ok(accountService.getById(id));
+    }
+
+}
