a48b0741d78b39787679d1757d9adeeb33fda080
Muhammed <muhammedihsansolak@gmail.com>
2023-12-13 11:52:18 +0300

    Module 25 created. Dockerfile DONE

---
+++ spring-25-Docker/src/main/java/com/cydeo/Spring25DockerApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring25DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring25DockerApplication.class, args);
+    }
+
+}
---
+++ spring-25-Docker/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        
+        return "home.html";
+    }
+
+
+
+
+
+}
